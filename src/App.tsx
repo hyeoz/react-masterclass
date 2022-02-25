2172688e5f9b478ee7e88891411027ee2bda9264
hyeoz <jenny970203@gmail.com>
2022-02-25 19:55:13 +0900

    chapter.3 typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`
 
 function App() {
+  const [value, setValue] = useState("");
+  // event 의 type 설정해주기
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value); // javascript 에서 target 으로 사용한 것과 같음
+    const {currentTarget: {value}} = event;
+    setValue(value)
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container>
+    <H1>protected</H1>
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="username" value={value} onChange={onChange} />
+      <button>Log In</button>
+    </form>
+  </Container>
   );
 }
 
